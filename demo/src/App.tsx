57a93c9d623cea38e1dd31c2cb560898349435a5
mahziyar-es <mahziyar.esmaili.dev@gmail.com>
2023-05-16 14:10:46 +0430

    build bug fix

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -1,6 +1,6 @@
 
 import { useEffect, useRef, useState } from 'react'
-import {Notify, Gamon, Confirm, Picker, TabBar, Progressbar, InputBasic, InputCheckbox, InputRadio, InputRange, InputSelect, InputTextarea, InputToggle, InputFile, Stepper, Button, Loading, Row, Col, GrabScroll, Layout, InputDate, Sheet, Carousel} from '../../src/index'
+import {Notify, Gamon, Confirm, Picker, TabBar, Progressbar, InputBasic, InputCheckbox, InputRadio, InputRange, InputSelect, InputTextarea, InputToggle, InputFile, Stepper, Button, Loading, Row, Col, GrabScroll, Layout, InputDate, Sheet, Carousel} from '../../dist/esm'
 import "./App.css"
 
 
